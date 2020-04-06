b352a7c22f5c1b5d3b668b0cf47adfefe1a81579
Dimitris Gounaris <dimitrisgounaris@20LDN-MAC257.local>
2020-04-06 15:39:05 +0100

    Fix the issues of the api call

--- common/net/jira/getissue/action.js
+++ common/net/jira/getissue/action.js
@@ -9,14 +9,15 @@ class JiraGetIssueAction {
   }
 
   async execute () {
-    console.log(`Issue to search jira for: ${this.jiraIssue}`)
+    console.log(`Issue to search jira for: ${this.issue}`)
     let config = {
       headers: {
           'Authorization': `Basic ${this.token}`,
       }
     }
 
-    return await axios.get(`${this.baseurl}/rest/api/3/issue/${this.issue}`, config);
+    const response = await axios.get(`${this.baseUrl}/rest/api/3/issue/${this.issue}`, config);
+    return response.data;
   }
 }
 
